0322b5e8a5053fe3628a90ad2aa88d501aeb17f1
Hannah A Pliner <hpliner@gmail.com>
2019-10-07 15:49:24 +0100

    allow changing of cluster_extend parameters: #12

--- tests/testthat/test-classify_cells.R
+++ tests/testthat/test-classify_cells.R
@@ -78,4 +78,43 @@ test_that("classify_cells error messages work", {
                      "db?"))
 })
 
+set.seed(10)
+new_cds <- classify_cells(test_cds, test_classifier,
+                          db = org.Hs.eg.db,
+                          rank_prob_ratio = 1.5,
+                          cluster_extend = TRUE,
+                          cds_gene_id_type = "SYMBOL",
+                          cluster_extend_max_frac_unknown = .4,
+                          cluster_extend_max_frac_incorrect = .5)
+
+set.seed(10)
+new_cds2 <- classify_cells(test_cds, test_classifier,
+                          db = org.Hs.eg.db,
+                          rank_prob_ratio = 1.5,
+                          cluster_extend = TRUE,
+                          cds_gene_id_type = "SYMBOL",
+                          cluster_extend_max_frac_unknown = .95,
+                          cluster_extend_max_frac_incorrect = .5)
+
+test_that("classify_cells extension parameters work", {
+  expect_identical(counts(new_cds), counts(test_cds))
+  expect_identical(rowData(new_cds), rowData(test_cds))
+  expect_equal(sum(colData(new_cds)$cell_type == "B cells"), 207)
+  expect_equal(sum(colData(new_cds)$cell_type == "CD4 T cells"), 129)
+  expect_equal(sum(colData(new_cds)$cell_type == "CD8 T cells"), 61)
+  expect_equal(sum(colData(new_cds)$cell_type == "T cells"), 164)
+  expect_equal(sum(colData(new_cds)$cluster_ext_type == "B cells"), 207)
+  expect_equal(sum(colData(new_cds)$cluster_ext_type == "CD4 T cells"), 1)
+  expect_equal(sum(colData(new_cds)$cluster_ext_type == "T cells"), 400)
+
+  expect_identical(counts(new_cds2), counts(test_cds))
+  expect_identical(rowData(new_cds2), rowData(test_cds))
+  expect_equal(sum(colData(new_cds2)$cell_type == "B cells"), 207)
+  expect_equal(sum(colData(new_cds2)$cell_type == "CD4 T cells"), 129)
+  expect_equal(sum(colData(new_cds2)$cell_type == "CD8 T cells"), 61)
+  expect_equal(sum(colData(new_cds2)$cell_type == "T cells"), 164)
+  expect_equal(sum(colData(new_cds2)$cluster_ext_type == "B cells"), 400)
+  expect_equal(sum(colData(new_cds2)$cluster_ext_type == "CD4 T cells"), 400)
+  expect_equal(sum(colData(new_cds2)$cluster_ext_type == "T cells"), 0)
+})
 
